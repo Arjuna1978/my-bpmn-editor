c222b7b728ce7765979b7683c6e7d05f1bf6a0f9
Arjuna Vijayanayagam <99155024+Arjuna1978@users.noreply.github.com>
2026-04-13 11:28:49 +1000

    CSS update

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import BpmnModeller from './BpmnModeller';
 function App() {
   return (
     <div className="App">
-      <header style={{ padding: '10px', backgroundColor: '#282c34', color: 'white' }}>
-        <h1 style={{ fontSize: '1.2rem', margin: 0 }}>Arjuna's BPMN Workflow Editor</h1>
+      <header className="app-header">
+        <h1 className='app-title'>Arjuna's BPMN Workflow Editor</h1>
       </header>
       <main>
         <BpmnModeller />
