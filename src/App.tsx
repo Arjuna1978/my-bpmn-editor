8d1b3df58d355bb601674af276cbec12ce95fa0b
Arjuna1978 <99155024+Arjuna1978@users.noreply.github.com>
2026-05-01 16:05:49 +1000

    changes to browser tag name

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import BpmnModeller, { BpmnModellerHandle } from "./services/BpmnModeller";
 import { LoadButton } from "./components/LoadButton";
 import { SaveButton } from "./components/SaveButton";
 import logo from "./resources/logo.svg";
-import downImage from "./resources/Down.svg"
+import downImage from "./resources/down.svg"
 import "bpmn-js/dist/assets/diagram-js.css";
 import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
 import "bpmn-js/dist/assets/bpmn-js.css";
@@ -14,9 +14,7 @@ import "bpmn-js/dist/assets/bpmn-js.css";
 function App() {
 
   const modellerRef = useRef<BpmnModellerHandle>(null);
-  // this flag is used to protect against loading the canvas before the reference model is loaded
     const hasImportedRef = useRef(false);
-  //note that my-process.bpmn is a starter file that we provide to give people a kick start
   const [fileName, setFileName] = useState<string>("my-process.bpmn");
 
 
@@ -27,19 +25,18 @@ function App() {
     }
   }, []);
 
-  // callback for file loading service
   function handleLoadXml({ xml, fileName }: { xml: string; fileName: string }) {
     setFileName(fileName);
     return modellerRef.current?.importXml(xml);
   }
-//Callback for Export file as BPMN
+  
   async function handleExportBPMN() {
     const savedFileName = await modellerRef.current?.exportXml(fileName);
     if (savedFileName) {
       setFileName(savedFileName);
     }
   }
-//Callback for Export file as BPMN
+
   async function handleExportSVG() {
     const savedFileName = await modellerRef.current?.exportSVG(fileName);
     if (savedFileName) {
